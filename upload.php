970c7b61451f8e9fdb176f96c7bc291a42cf3f1c
Jonathasbastosjr <jonathasjuniorsp@hotmail.com>
2025-10-24 10:02:07 -0300

    Update upload.php

--- upload.php
+++ upload.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 header('Content-Type: application/json; charset=utf-8');
 $maxSize = 10 * 1024 * 1024; // 10MB
 $allowed = ['image/jpeg'=>'jpg','image/png'=>'png','image/webp'=>'webp'];
